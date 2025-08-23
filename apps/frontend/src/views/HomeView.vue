7fb722c80d326f50e607100d09ec2a473ca0225d
Alessandro Curcu <aleqrq@gmail.com>
2025-08-23 11:59:32 +0200

    feat: crea e  aggiungi il pacchetto '@qrq-repo/ui' e utilizza il componente Button in HomeView.vue

--- apps/frontend/src/views/HomeView.vue
+++ apps/frontend/src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <script setup lang='ts'>
+import { Button } from '@qrq-repo/ui';
 import { ref } from 'vue';
+import '@qrq-repo/ui/styles';
 
 const message = ref('Hello, World!');
 </script>
@@ -9,5 +11,8 @@ const message = ref('Hello, World!');
     <h1 class="text-3xl font-bold underline bg-amber-50 px-2">
       {{ message }}
     </h1>
+    <Button variant="destructive" size="lg">
+      Delete Item
+    </Button>
   </div>
 </template>
